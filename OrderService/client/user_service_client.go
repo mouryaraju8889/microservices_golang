bdeaa676fa7e3d7e6d6a2c8fa0ca4e887ba5c0bb
mouryaraju8889 <crce.8889.ce@gmail.com>
2025-04-19 22:05:47 +0530

    kubernetes |  ingress changes

--- OrderService/client/user_service_client.go
+++ OrderService/client/user_service_client.go
@@ -1,34 +1,34 @@
 package client
 
 import (
-    "encoding/json"
-    "net/http"
-    "orderservice/model"
+	"encoding/json"
+	"net/http"
+	"orderservice/model"
 )
 
 func GetAllUsers() ([]model.User, error) {
-    resp, err := http.Get("http://userservice:8080/users") // Kubernetes DNS name
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get("http://127.0.0.1:80/users") // Kubernetes DNS name
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    var users []model.User
-    err = json.NewDecoder(resp.Body).Decode(&users)
-    return users, err
+	var users []model.User
+	err = json.NewDecoder(resp.Body).Decode(&users)
+	return users, err
 }
 
 func UserExists(userID int) (bool, error) {
-    users, err := GetAllUsers()
-    if err != nil {
-        return false, err
-    }
+	users, err := GetAllUsers()
+	if err != nil {
+		return false, err
+	}
 
-    for _, u := range users {
-        if u.ID == userID {
-            return true, nil
-        }
-    }
+	for _, u := range users {
+		if u.ID == userID {
+			return true, nil
+		}
+	}
 
-    return false, nil
+	return false, nil
 }
